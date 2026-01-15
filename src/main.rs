a15dd24d3bee23569b722b1143393ab4af12c56c
unknown <2921323707@qq.com>
2026-01-15 16:45:51 +0800

    引入cmp方法并使用枚举Ordering进行两个数的比较

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("猜数!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let secret_number = rand::thread_rng().gen_range(1, 101); //前闭后开
     println!("秘密数字是: {}", secret_number);
 
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
     println!("你猜的数是: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("太小了!"),
+        Ordering::Greater => println!("太大了!"),
+        Ordering::Equal => println!("你赢了!"),
+    }
 }
