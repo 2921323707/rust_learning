47d8c2b27653cbdce0a100bda671db9eb1456bb3
unknown <2921323707@qq.com>
2026-01-15 16:32:26 +0800

    引入rand crate包并尝试生成随机数

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("猜数!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("秘密数字是: {}", secret_number);
+
     let mut guess = String::new();
     io::stdin()
         .read_line(&mut guess)
